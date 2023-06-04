407fb3ebdeb42b420f704be2a6087f8724f1045a
Idan Boros <iboros445@gmail.com>
2023-06-04 16:47:37 +0300

    added option to change playlist name

--- discover-everyweek.py
+++ discover-everyweek.py
@@ -1,6 +1,7 @@
 import playlists
 import spotipy
 import argparse
+import os
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -9,17 +10,20 @@ SCOPE = ["playlist-modify-private","playlist-read-private"]
 
 def argument_parsing():
     parser = argparse.ArgumentParser(description='Add discover weekly into another playlist')
-    parser.add_argument('--playlist_name', help='name of new playlist to create, defaults to "discovery"', default="Discovery")
-    parser.add_argument("--require_new_playlist", action=argparse.BooleanOptionalAction, default=True, help="Should create new playlist (will also be used in future) or use old one")
-    # TODO: add functionality that can use old playlist with given name (we give a name in playlist_name and add to it.)
+    parser.add_argument('--set_playlist_name', help='name of new playlist to create, defaults to "Discovery"')
+    parser.add_argument("--require_new_playlist", action=argparse.BooleanOptionalAction, default=False, help="Should create new playlist (will also be used in future) or use old one")
     args = parser.parse_args()
     return args
 
 def main():
-    args = argument_parsing()
     load_dotenv()
+    args = argument_parsing()
+    if args.set_playlist_name:
+        playlists.set_playlist_name(args.set_playlist_name)
+        print(">>INFO: Playlist name set correctly. please run again!")
+        exit(0)
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
-    playlist_to_move_to_id = playlists.get_or_create_playlist(sp, args.playlist_name)
+    playlist_to_move_to_id = playlists.get_or_create_playlist(sp, os.getenv("DISCOVER_EVERYWEEK_NAME"))
     discover_weekly_id = playlists.get_discover_weekly(sp)
     playlists.copy_one_playlist_to_another(sp ,discover_weekly_id, playlist_to_move_to_id)
 
